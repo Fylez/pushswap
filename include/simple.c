5b6df59a6c543c150c8e15d0eb6763b5d7fa4afa
fylez <superliamzaengel@gmail.com>
2024-01-12 00:48:09 +0100

    yippy

--- include/simple.c
+++ include/simple.c
@@ -6,7 +6,7 @@
 /*   By: lzaengel <lzaengel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 22:53:39 by lzaengel          #+#    #+#             */
-/*   Updated: 2024/01/11 23:36:19 by lzaengel         ###   ########.fr       */
+/*   Updated: 2024/01/12 00:10:15 by lzaengel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 void	three_numbers(t_list **lst)
 {
+	t_list	*next;
+	t_list	*node;
+
+	node = *lst;
+	next = node -> next;
+	if (node->content > next->content && node->content > next->next->content)
+	{
+		if (next -> next > next)
+			ra(lst);
+		else
+		{
+			ra(lst);
+			sa(*lst);
+		}
+	}
+	if (is_sorted(*lst) == 1)
+		return ;
 	sa(*lst);
 	if (is_sorted(*lst) == 1)
 		return ;
